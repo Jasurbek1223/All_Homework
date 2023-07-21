8a551f8924575bc7188d132899e015a0a9ebc982
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-07-21 17:22:53 +0500

    N9 Homework joylandi

--- N8-HT1/Program.cs
+++ N8-HT1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //Listga 3 ta misol
+            //Listga 
             /*
             1) Savatcha yani online magazinlarda narsa sotib olsak u savatchaga tushadi, biz unga yana narsa qo'shishimiz,
                 o'chirib tashlashimiz, yokida tahrirlashimiz mumkun(misol un rangini o'zgartirishimiz),
@@ -13,17 +13,38 @@
             3) Online sinf jurnali yani unda ham list korinishida saqlash qulay funksionalligi yuqori bo'lishi uchun 
                 
              */
+
             // Stack LIFO 
             /*
-             1) Misol uchun bazi saytlarda registratsiya yani ruyhatdan otishda ketma ketlik bor aval nomer keyin google akaunt 
+             * Misol uchun bazi saytlarda registratsiya yani ruyhatdan otishda ketma ketlik bor aval nomer keyin google akaunt 
                 keyin nicname shu kabi shu payt hato ketsa orqaga qaytib oldingi amaliyotni to'girlashimiz mumkin 
                 shu amal ham stackga misol bo'la oladi.Oxirgi amalni birinchi bo'lib to'g'irlaymiz
-            2) Telefonda menu ketma ketligi yani bir oynadan boshqa oynaga kirish uchu aval undan chiqish lozim shu holatda ham stackdan foydalansak bo'ladi
-            3) Qo'ng'iroqlar to'plami
+             * Telefonda menu ketma ketligi yani bir oynadan boshqa oynaga kirish uchu aval undan chiqish lozim shu holatda ham stackdan foydalansak bo'ladi
+             * Qo'ng'iroqlar to'plami
              */
 
+            // Queue
+            /*
+             * O'rinlar qatori
+             * Bankdagi navbat
+             * ish o'rinlarida navbat
+             * elektron suhbatga yozilish
+             */
 
+            // LinkedList
+            /*
+             * N ta musiqa ichidan o'zimiz yaratgan musiqiy playlist u holatda linked listga misol bo'la oladi
+             * Veb sahifalar yani qaysidir veb saytga kirganda unga mano jihatdan o'xshash boshqa sahifa korsatiladi va u
+                ham linked listga misol bola oladi
+             * Telefon kitobchasi yani har bir foydalanuvchi malumotlari ketma ketligi uchun linked list ishlatsa bo'ladi
+             */
 
+            //Dictionary
+            /*
+             * elektron lug'at
+             * Kontakt malumotlari ism bn birga yozilsa
+             * Mashina madellari haqida ilova 
+             */
 
 
 
